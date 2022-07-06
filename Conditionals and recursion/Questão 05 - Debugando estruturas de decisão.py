4dd6e59f2c51c6daebd61b8d37bd2220f29f8dd8
GuilhermeSams <guilhermeps3670@gmail.com>
2022-07-06 15:40:49 -0300

    update problem 5

--- Conditionals and recursion/Questão 05 - Debugando estruturas de decisão.py
+++ Conditionals and recursion/Questão 05 - Debugando estruturas de decisão.py
@@ -1,4 +1,4 @@
-indiceReclamacao = int(input())
+"""indiceReclamacao = int(input())
 percentReclamResolPrim = int(input())
 percentCliCancel = int(input())
 indiceIndisponibilidade = int(input())
@@ -13,15 +13,21 @@ else:
 
 
 if percentCliCancel >= 10:
-    if canceladoPorProblema != 0:
+    if canceladoPorProblema == 1:
+        indice = indice + 80
+        print(f'{indice}')
+    elif canceladoPorProblema == 0:
         indice = indice + 80
         print(f'{indice}')
     else:
         indice = indice - 30
         print(f'{indice}')
 else:
-    if canceladoPorProblema != 0:
+    if canceladoPorProblema == 1:
         indice = indice + 50
+        print(f'{indice}')
+    elif canceladoPorProblema == 0:
+        indice = indice + 80
         print(f'{indice}')
     else:
         indice = indice - 10
@@ -34,3 +40,37 @@ if indiceIndisponibilidade >= 10:
 else:
     indice = indice - 20
     print(f'{indice}')
+"""
+
+indiceReclamacao = int(input())
+percentReclamResolPrim = int(input())
+percentCliCancel = int(input())
+indiceIndisponibilidade = int(input())
+canceladoPorProblema = int(input())
+
+if percentReclamResolPrim >= 60:
+    indice = indiceReclamacao - 5
+else:
+    indice = indiceReclamacao + 15
+print(f'{indice}')
+
+if percentCliCancel >= 10:
+    if canceladoPorProblema == 1:
+        indice = indice + 80
+
+    elif canceladoPorProblema == 0:
+        indice = indice - 30
+else:
+    if canceladoPorProblema == 1:
+        indice = indice + 50
+
+    elif canceladoPorProblema == 0:
+        indice = indice - 10
+
+print(f'{indice}')
+
+if indiceIndisponibilidade >= 10:
+    indice = indice + 70
+else:
+    indice = indice - 20
+print(f'{indice}')
